846ef90f74b7507e87927dc3a28385aa823a4407
Remy <ext.modis.remy.juanes@sncf.fr>
2018-02-01 11:06:34 +0100

    le fichier de config est créer après l'installation dans le dossier racine

---
+++ postInstall.js
@@ -0,0 +1,7 @@
+/**
+ * Created by PRJS12821 on 01/02/2018.
+ */
+const fctSys = require("./fonctionSystem");
+//CRÉATION DU FICHIER DE CONFIG À LA RACINE
+console.log("Création du fichier de configuration à la racine du répertoire.");
+fctSys._createConfigFile();
